07b2910a713d375cd474b2886d2de78cc1dc01c9
FredymarLeon <fredymar@gmail.com>
2024-10-11 22:50:29 -0300

    Se finalizou a aplicação de BOTÔES, como práctica do conteudo BOTÔES como componentes básicos de UI. Android Studio | Kotlin

--- app/src/main/java/com/fleon/botoescomponentesbasicosdeui/MainActivity.kt
+++ app/src/main/java/com/fleon/botoescomponentesbasicosdeui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.fleon.botoescomponentesbasicosdeui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val button: Button by lazy {findViewById(R.id.button)}
+    private val imageButton: ImageButton by lazy {findViewById(R.id.imageButton)}
+    private val toggleButton: ToggleButton by lazy {findViewById(R.id.toggleButton)}
+    private val fab: FloatingActionButton by lazy {findViewById(R.id.fab)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,50 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupButton()
+        setupImageButton()
+        setupToggleButton()
+        setupFAB()
+
     }
+
+    private fun setupButton() {
+        button.setOnClickListener{
+            Toast.makeText(this, "Clique rápido no Button", Toast.LENGTH_SHORT).show()
+        }
+        button.setOnLongClickListener {
+            Toast.makeText(this, "Clique longo no Button", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+    }
+
+    private fun setupImageButton() {
+        imageButton.setOnClickListener {
+            Toast.makeText(this, "Clique rápido no Image Button", Toast.LENGTH_SHORT).show()
+        }
+
+        imageButton.setOnLongClickListener{
+            Toast.makeText(this, "Clique longo no Image Button", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+    }
+
+    private fun setupToggleButton() {
+        toggleButton.setOnCheckedChangeListener {_, isChecked ->
+            if(isChecked){
+                Toast.makeText(this, "Toggle ligado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Toggle desligado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setupFAB() {
+        fab.setOnClickListener {
+            Toast.makeText(this, "Clique no FAB: ALERTA!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
